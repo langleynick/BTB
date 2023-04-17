3950f5e8f84cb520ad4b71ddc5aa551157c4f019
langleynick <w0106967@myscc.ca>
2023-04-17 19:49:19 -0400

    init of various other pages

---
+++ btbwebsite/pages/contact.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+
+export default function Contact() {
+    return (
+        <>
+            <Layout
+                title="About - BTB"
+                description="About Us"
+                >
+                
+            </Layout>
+        </>
+    )
+}
---
+++ btbwebsite/pages/products.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout"
+
+export default function Products() {
+    return (
+        <>
+            <Layout
+                title="About - BTB"
+                description="About Us"
+                >
+                
+            </Layout>
+        </>
+    )
+}
